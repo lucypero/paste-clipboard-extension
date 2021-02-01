5e61931b6f00648af3b20cee6fb2c1f89647a7c0
Lucy <lucyperopero@gmail.com>
2021-02-01 17:38:56 -0300

    modify method when pasting copied text to the notepad

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,29 @@
 var port = chrome.runtime.connect({name: "knockknock"});
 
-let displayed_lines = []
-let max_displayed_lines = 5
 
-port.onMessage.addListener(function(msg) {
+// the new way just adds the line to whatever was in the notepad.
+// 
+function new_way(msg) {
+
+	let max_character_count = 2000
+
+  var ta = document.getElementById("ta");
+	ta.innerHTML += '<br><br>' + msg
+	if(ta.innerText.length > max_character_count) {
+		let difference = ta.innerText.length - max_character_count
+		ta.innerText = ta.innerText.slice(difference)
+	}
+
+
+	window.scrollTo(0,document.body.scrollHeight);
+}
+
+// the old way deleted everything in the notepad to insert the copied lines.
+// this is annoying when you have something typed on it that you don't want to delete.
+function old_way(msg) {
+
+	let displayed_lines = []
+	let max_displayed_lines = 20
   // console.log("recieved message:", msg)
   var ta = document.getElementById("ta");
 	ta.innerHTML = ''
@@ -17,4 +37,7 @@ port.onMessage.addListener(function(msg) {
 	//display all displayed_lines in order
 	for(line of displayed_lines)
 		ta.innerHTML += line + '<br><br>'
-});
+}
+
+
+port.onMessage.addListener(new_way);
